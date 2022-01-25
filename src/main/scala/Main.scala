22f29f421269c959a58e00f53e6e4ad679b927e7
buhe <bugu1986@126.com>
2022-01-25 20:58:27 +0800

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,14 @@
 object Main extends App {
   println("Hello, World!")
+  val y = XiaoM
+  println(y.a)
+}
+
+class XiaoM {
+  val x = 666
+}
+
+object XiaoM {
+  val z = new XiaoM
+  val a = z.x
 }
