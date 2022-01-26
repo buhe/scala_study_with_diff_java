b8b7153fcc2662f90b06d457fcf8302cd2770fcf
buhe <bugu1986@126.com>
2022-01-26 11:16:18 +0800

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,16 +2,22 @@ object Main extends App {
   println("Hello, World!")
   val y = XiaoM
   println(y.a)
+  val z = new XiaoH(31)
+  z.p()
 }
 
-class XiaoM {
+class XiaoM(val c: Int) {
   val x = 666
 
   def d(s: String) = println(s)
 }
 
 object XiaoM {
-  val z = new XiaoM
+  val z = new XiaoM(22)
   val a = z.x
   z d "1"
+}
+
+class XiaoH(b: Int) extends XiaoM(b) {
+  def p() = println(c)
 }
